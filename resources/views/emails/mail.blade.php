192e11080f0b94fc75d743c055c495c3c3e62bc6
IBeee13 <iilhambiintang@gmmail.com>
2026-06-16 17:18:47 +0800

    update mail

--- resources/views/emails/mail.blade.php
+++ resources/views/emails/mail.blade.php
@@ -85,7 +85,7 @@
                                                 {{-- Avatar --}}
                                                 <td width="48" valign="top">
                                                     <div
-                                                        style="width:44px; height:44px; background-color:#18254D; border-radius:999px; text-align:center; align-items: center; line-height:44px;">
+                                                        style="width:44px; height:44px; background-color:#18254D; border-radius:999px; text-align:center; justify-content: center; line-height:44px;">
                                                         <span style="font-size:18px; color:#C7A578; font-weight:700;">
                                                             {{ strtoupper(substr($data['name'], 0, 1)) }}
                                                         </span>
